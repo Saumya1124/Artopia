4ac52dcfdc9f58b383a1eee51ee7adf663eb0101
Saumya Jaiswal <24jaiswalsaumya@gmail.com>
2021-02-04 17:18:21 +0530

    Front-End

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { SectionComponent } from './section/section.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavComponent,
+    SectionComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
